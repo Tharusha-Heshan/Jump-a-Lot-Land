be86e50bceaf8e9b7fc74eaad55f8bad90c7ef9f
Imesh Munasinghe <imesh5lk@gmail.com>
2026-06-13 18:54:20 +0530

    Refactor Level 2 logic into separate Level2Logic.cpp and .h files

---
+++ Level2Logic.h
@@ -0,0 +1,12 @@
+#ifndef LEVEL2LOGIC_H
+#define LEVEL2LOGIC_H
+
+void initLevel2Particles();
+
+void drawLevel2Background();
+
+void drawLevel2Particles();
+
+void drawLevel2Tile(float x, float y, int type);
+
+#endif
